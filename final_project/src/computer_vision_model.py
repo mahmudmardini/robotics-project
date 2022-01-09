fedf86302b2fe7d30cd593ead16412bdfab5d8b0
Hasan-Elhussein <hasan.elhussien.97@gmail.com>
2022-01-09 23:02:10 +0300

    Add project files

---
+++ final_project/src/computer_vision_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+
+# Load the model
+modelPath = "/home/hassan/catkin_ws/src/final_project/src/keras_model/keras_model.h5"
+model = load_model(modelPath)
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+# Replace this with the path to your image
+imgPath = "/home/hassan/catkin_ws/src/final_project/turtlebot_photos/left/frame0001.jpg"
+image = Image.open(imgPath)
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+# Load the image into the array
+data[0] = normalized_image_array
+
+# run the inference
+prediction = model.predict(data)
+#print("left: " + str(prediction[0][0]) + "\nright: " + str(prediction[0][1]))
+
+if prediction[0][0] >= prediction[0][1]:
+    print("It's a turn left sign " + str(prediction[0][0]))
+else:
+    print("It's a turn right sign " + str(prediction[0][1]))
